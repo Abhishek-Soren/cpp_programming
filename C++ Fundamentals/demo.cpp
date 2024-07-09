e72df008c4ef028d12954c45a2b33e321e093a6e
Abhishek Soren <abhisheksoren95@gmail.com>
2024-07-09 20:48:24 +0530

    update

--- C++ Fundamentals/demo.cpp
+++ C++ Fundamentals/demo.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define ld long double
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define pb push_back
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+
 int main()
 {
-  
+  cout << "Hello World";
   return 0;
 }
